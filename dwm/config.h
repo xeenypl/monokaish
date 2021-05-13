72d127e0ccdbafa014f8ae9f34cbf024c977c5ab
xeeny <xeeny@wp.pl>
2021-05-13 18:41:31 +0200

    Addet lstack layout

--- dwm/config.h
+++ dwm/config.h
@@ -37,10 +37,44 @@ static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] *
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+void lstack(Monitor *m) {
+    unsigned int i, n, h, mw, my, ty;
+    Client *c;
+
+    for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+    if (n == 0)
+        return;
+
+    if (n > m->nmaster) {
+        mw = m->nmaster ? m->ww * m->mfact : 0;
+        for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+            if (i < m->nmaster) {
+                h = (m->wh - my) / (MIN(n, m->nmaster) - i);
+                resize(c, m->wx + mw, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
+                if (my + HEIGHT(c) < m->wh)
+                    my += HEIGHT(c);
+            } else {
+                h = (m->wh - ty) / (n - i);
+                resize(c, m->wx, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
+                if (ty + HEIGHT(c) < m->wh)
+                    ty += HEIGHT(c);
+            }
+    } else {
+        mw = m->ww;
+        for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+            h = (m->wh - my) / (MIN(n, m->nmaster) - i);
+            resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
+            if (my + HEIGHT(c) < m->wh)
+                my += HEIGHT(c);
+        }
+    }
+}
+
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    { "S",      tile },    /* first entry is default */
-    { "F",      NULL },    /* no layout function means floating behavior */
+    { "R",      tile },    /* first entry is default */
+    { "L",      lstack },    /* first entry is default */
+    { "",       NULL },    /* no layout function means floating behavior */
     { "M",      monocle },
 };
 
